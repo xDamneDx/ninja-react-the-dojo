0e01c3c41673829be809affc2a286010c898baa9
xDamneDx <xdamnedx@gmail.com>
2022-05-17 15:43:05 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Create from "./pages/create/Create";
 import Project from "./pages/project/Project";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
+          <Navbar />
           <Switch>
             <Route path="/" exact>
               <Dashboard />
