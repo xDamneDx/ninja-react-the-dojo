4a73178c210dc01043f87efd954dc2129a21ba67
xDamneDx <xdamnedx@gmail.com>
2022-05-17 15:15:36 +0300

    Added routes setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// Styles:
 import "./App.css";
 
+// Pages and components:
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Project from "./pages/project/Project";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route path="/" exact>
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
