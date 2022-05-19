7924242af430b6cef2aefb5c96fa05eb7a994c6d
xDamneDx <xdamnedx@gmail.com>
2022-05-19 10:09:35 +0300

    Added logout functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // Styles & images:
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 
 export default function Navbar() {
+  const { logout, isPending } = useLogout();
+
   return (
     <div className="navbar">
       <ul>
@@ -19,7 +22,16 @@ export default function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
     </div>
