df92b55159db7586271b53b9628e2e07cb6d90ac
xDamneDx <xdamnedx@gmail.com>
2022-05-19 14:23:49 +0300

    Added OnlineUsers component

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Project from "./pages/project/Project";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar";
+import OnlineUsers from "./components/OnlineUsers";
 
 function App() {
   const { user, authIsReady } = useAuthContext();
@@ -46,6 +47,7 @@ function App() {
               </Route>
             </Switch>
           </div>
+          {user && <OnlineUsers />}
         </BrowserRouter>
       )}
     </div>
