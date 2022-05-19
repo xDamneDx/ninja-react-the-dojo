c0d3e414b29617ae5c14a744fd2e2d7830e57070
xDamneDx <xdamnedx@gmail.com>
2022-05-19 14:22:20 +0300

    Added OnlineUsers component

---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,23 @@
+import { useCollection } from "../hooks/useCollection";
+import Avatar from "./Avatar";
+
+// Styles:
+import "./OnlineUsers.css";
+
+export default function OnlineUsers() {
+  const { error, documents } = useCollection("users");
+
+  return (
+    <div className="user-list">
+      <h2>All users</h2>
+      {error && <p className="error">{error}</p>}
+      {documents &&
+        documents.map((user) => (
+          <div key={user.id} className="user-list-item">
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  );
+}
