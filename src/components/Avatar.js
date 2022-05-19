188eb3bf190777c915ed91cbf25922d1f0fad8de
xDamneDx <xdamnedx@gmail.com>
2022-05-19 13:23:48 +0300

    Created Avatar component

---
+++ src/components/Avatar.js
@@ -0,0 +1,10 @@
+// Styles:
+import "./Avatar.css";
+
+export default function Avatar({ src }) {
+  return (
+    <div className="avatar">
+      <img src={src} alt="user avatar" />
+    </div>
+  );
+}
