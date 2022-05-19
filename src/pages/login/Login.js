a799c9b0ac78969697ec1e007a134b7fb14da979
xDamneDx <xdamnedx@gmail.com>
2022-05-19 10:42:08 +0300

    Added login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,47 @@
+import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+
 // Styles:
 import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          name={password}
+        />
+      </label>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <div className="error">{error}</div>}
+    </form>
+  );
 }
