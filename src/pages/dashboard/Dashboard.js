b818740c388f6d54bfd85f273138457ed0e1da3f
xDamneDx <xdamnedx@gmail.com>
2022-05-23 11:11:12 +0300

    Added fetching projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,19 @@
+import { useCollection } from "../../hooks/useCollection";
+
 // Styles:
 import "./Dashboard.css";
 
+// Components:
+import ProjectList from "../../components/ProjectList";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
