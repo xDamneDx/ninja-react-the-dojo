c06d56fdb0961055d6ea3a2f14001a7e67b92853
xDamneDx <xdamnedx@gmail.com>
2022-05-23 16:34:59 +0300

    Added markup for displaying projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,29 @@
+import { Link } from "react-router-dom";
+
 // Styles:
-import "./ProjectList";
+import "./ProjectList.css";
+
+// Components:
+import Avatar from "./Avatar";
 
 export default function ProjectList({ projects }) {
   return (
-    <div>
+    <div className="project-list">
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
