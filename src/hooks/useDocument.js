9518ed75de0e4d84070e65e0eb54a9453edfc845
xDamneDx <xdamnedx@gmail.com>
2022-05-23 17:48:09 +0300

    Added custom error if document doesn't exists

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -11,8 +11,12 @@ export const useDocument = (collection, id) => {
 
     const unsubscribe = ref.onSnapshot(
       (snapshot) => {
-        setDocument({ ...snapshot.data(), id: snapshot.id });
-        setError(null);
+        if (snapshot.data()) {
+          setDocument({ ...snapshot.data(), id: snapshot.id });
+          setError(null);
+        } else {
+          setError("No such document exists");
+        }
       },
       (error) => {
         console.log(error.message);
