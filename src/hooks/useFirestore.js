b10d1ef8899b78dff933021d164fd694a66369f9
xDamneDx <xdamnedx@gmail.com>
2022-05-24 15:05:21 +0300

    Added updateDocument function

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -26,6 +26,13 @@ const fireStoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case "UPDATED_DOCUMENT":
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
     case "ERROR":
       return {
         isPending: false,
@@ -82,9 +89,26 @@ export const useFirestore = (collection) => {
     }
   };
 
+  // update document:
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: "IS_PENDING" });
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates);
+      dispatchIfNotCancelled({
+        type: "UPDATED_DOCUMENT",
+        payload: updatedDocument,
+      });
+      return updatedDocument;
+    } catch (error) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: error.message });
+      return null;
+    }
+  };
+
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { addDocument, deleteDocument, response };
+  return { addDocument, deleteDocument, updateDocument, response };
 };
