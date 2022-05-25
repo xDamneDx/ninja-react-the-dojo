287a27d1a4be63955d0dc5d1174947c8ce7bfd02
xDamneDx <xdamnedx@gmail.com>
2022-05-25 10:27:24 +0300

    Added comments section

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,9 @@ import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
 
+// Components:
+import Avatar from "../../components/Avatar";
+
 export default function ProjectComments({ project }) {
   const [newComment, setNewComment] = useState("");
   const { user } = useAuthContext();
@@ -30,6 +33,26 @@ export default function ProjectComments({ project }) {
   return (
     <div className="project-comments">
       <h4>Project Comments:</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                {/* TODO: Add date */}
+                <p>date here</p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
