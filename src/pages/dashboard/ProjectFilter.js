e3dda1e2afdfdc34f960eba21a6d6fc7ff70dbea
xDamneDx <xdamnedx@gmail.com>
2022-05-26 14:01:40 +0300

    Transfer filtering to Dashboard component

--- src/pages/dashboard/ProjectFilter.js
+++ src/pages/dashboard/ProjectFilter.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 const filterList = [
   "all",
   "mine",
@@ -9,12 +7,9 @@ const filterList = [
   "sales",
 ];
 
-export default function ProjectFilter() {
-  const [currentFilter, setCurrentFilter] = useState("all");
-
+export default function ProjectFilter({ currentFilter, changeFilter }) {
   const handleClick = (newFilter) => {
-    console.log(newFilter);
-    setCurrentFilter(newFilter);
+    changeFilter(newFilter);
   };
 
   return (
