f24f9e240855a335eb0aecc1838623a427fa60ba
xDamneDx <xdamnedx@gmail.com>
2022-05-26 11:10:47 +0300

    Added Mark as complete \ delete functionality

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,11 +1,25 @@
+import { useFirestore } from "../../hooks/useFirestore";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useHistory } from "react-router-dom";
+
 // Components:
 import Avatar from "../../components/Avatar";
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore("projects");
+  const { user } = useAuthContext();
+  const history = useHistory();
+
+  const handleClick = () => {
+    deleteDocument(project.id);
+    history.push("/");
+  };
+
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">
           Project due by: {project.dueDate.toDate().toDateString()}
         </p>
@@ -19,6 +33,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
