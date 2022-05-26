b35c8ac0c30b4a8929f316e73ff55ae5e46b5843
xDamneDx <xdamnedx@gmail.com>
2022-05-26 17:12:14 +0300

    Added functionality for filtering projects

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useCollection } from "../../hooks/useCollection";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // Styles:
 import "./Dashboard.css";
@@ -11,10 +12,36 @@ import ProjectFilter from "./ProjectFilter";
 export default function Dashboard() {
   const { documents, error } = useCollection("projects");
   const [currentFilter, setCurrentFilter] = useState("all");
+  const { user } = useAuthContext();
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case "all":
+            return true;
+          case "mine":
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          case "development":
+          case "design":
+          case "sales":
+          case "marketing":
+            console.log(document.category, currentFilter);
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -24,7 +51,7 @@ export default function Dashboard() {
         changeFilter={changeFilter}
       />
       {error && <p className="error">{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
